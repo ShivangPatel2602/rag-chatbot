0f7cbc91518b1c63cf84d237b4f604bdcb86b3f1
Shivang Patel <shivang.patel02062002@gmail.com>
2025-03-18 18:25:30 -0400

    updated urls

--- rag_chatbot_app/urls.py
+++ rag_chatbot_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import rag_chatbot_app_view
+from . import views
 
 urlpatterns = [
-    path('', rag_chatbot_app_view, name="rag_chatbot_app")
+    path('', views.rag_chatbot_app_view, name="rag_chatbot_app"),
+    path('ask_chatbot/', views.ask_chatbot, name='ask_chatbot')
 ]
